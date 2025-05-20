477e9dbae8d5e6c1fb8c433f9ce5e880c79fc0d5
RewelliOliveira <rewelli@alu.ufc.br>
2025-05-20 15:00:13 -0300

    outra fomra de fazer p dark mode

--- atividade3/script.js
+++ atividade3/script.js
@@ -6,3 +6,9 @@ toggle.addEventListener("change", () => {
     document.body.classList.remove("dark-mode");
   }
 });
+
+// outra forma de fazer o dark mode ou qualquer outra lofica de true or false
+// const toggle = document.getElementById("toggle");
+// toggle.addEventListener("change", () => {
+//   document.body.classList.toggle("dark-mode");
+// });
